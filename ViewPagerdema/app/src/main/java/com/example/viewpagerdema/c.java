a03b1acf26291bcbd6334d8265e7e66076eaa533
jeasonjay <809501346@qq.com>
2017-01-15 19:46:25 +0800

    工作用到的第一个技术点之ViewPager
    ViewPager+view(Activity)使用

---
+++ ViewPagerdema/app/src/main/java/com/example/viewpagerdema/c.java
@@ -0,0 +1,13 @@
+package com.example.viewpagerdema;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class c extends Activity {
+@Override
+public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.c);
+
+}
+}
